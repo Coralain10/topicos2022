066978729f9ad25e8ce253e3f878d96c840e3467
Coralain <103149767+Cora42@users.noreply.github.com>
2022-09-02 21:53:30 -0500

    agregando un comentario

--- core/src/com/mygdx/game/GameObjects/Ball.java
+++ core/src/com/mygdx/game/GameObjects/Ball.java
@@ -31,6 +31,7 @@ public class Ball extends Figure {
     }
 
     public void draw(ShapeRenderer shapeRenderer) {
+        //draw for the Ball
         super.draw(shapeRenderer);
         shapeRenderer.circle(x,y,size);
     }
